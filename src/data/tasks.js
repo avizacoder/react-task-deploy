ae4ca122cba10550dff367e4813f1e4f9d6ddb6f
avizaleonel <leonnelgma@gmail.com>
2022-11-26 12:11:36 -0400

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'My first task',
+        description: 'my firs task'
+    },
+    {
+        id: 1,
+        title: 'My second task',
+        description: 'my second task'
+    },
+    {
+        id: 2,
+        title: 'My third task',
+        description: 'my third task'
+    }
+]
